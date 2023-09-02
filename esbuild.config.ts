25c54b0f013f6a0cef363f35c26fb58485f16f98
aMediocreDad <amediocredad@outlook.com>
2023-09-02 16:34:20 +0200

    refactor(wip): ♻️ Initial move to modern css

--- esbuild.config.ts
+++ esbuild.config.ts
@@ -1,5 +1,4 @@
 import { context } from "esbuild";
-import { sassPlugin } from "esbuild-sass-plugin";
 import { existsSync } from "node:fs";
 import { rm } from "node:fs/promises";
 import { args } from "./tools/args-parser.js";
@@ -10,11 +9,10 @@ const templatePaths = await templatePathsPromise;
 const development = Object.hasOwn(args, "development");
 
 if (existsSync("./forbidden-lands.js")) await rm("./forbidden-lands.js");
-if (existsSync("./forbidden-lands.css")) await rm("./forbidden-lands.css");
 
 const ctx = await context({
 	bundle: true,
-	entryPoints: ["./src/forbidden-lands.ts", "./src/forbidden-lands.scss"],
+	entryPoints: ["./src/forbidden-lands.ts"],
 	outdir: "./",
 	format: "esm",
 	logLevel: "info",
@@ -26,24 +24,6 @@ const ctx = await context({
 	define: {
 		GLOBALPATHS: JSON.stringify(templatePaths),
 	},
-	plugins: [
-		sassPlugin({
-			logger: {
-				warn: () => "",
-			},
-		}),
-		{
-			name: "external-files",
-			setup(inBuild) {
-				inBuild.onResolve(
-					{ filter: /(\.\/assets|\.\/fonts|\/systems)/ },
-					() => {
-						return { external: true };
-					},
-				);
-			},
-		},
-	],
 });
 
 ctx.rebuild();
